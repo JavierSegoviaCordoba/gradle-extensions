7a8d5e020849ce651643199ed9e30f0ee9ac09d8
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-01-26 11:29:44 +0100

    Add test for `GradleTest.gradleTest`

--- integration-tests/test/kotlin/com/javiersc/integration/tests/GradleTestExtensionsTest.kt
+++ integration-tests/test/kotlin/com/javiersc/integration/tests/GradleTestExtensionsTest.kt
@@ -7,7 +7,12 @@ import kotlin.test.Test
 internal class GradleTestExtensionsTest : GradleTest() {
 
     @Test
-    fun `given a sandbox path when project builds then project's name is fake-file`() {
+    fun `GIVEN a null sandbox path WHEN project builds THEN project's name is an empty string`() {
+        gradleTest(sandboxPath = null) { name.shouldBe("") }
+    }
+
+    @Test
+    fun `GIVEN a sandbox path WHEN project builds THEN project's name is 'fake-file'`() {
         gradleTest(sandboxPath = "fake-file") { name.shouldBe("fake-file") }
     }
 }
