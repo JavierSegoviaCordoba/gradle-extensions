4f4b6bd9117d82b8af204f89d1b7210507a977d3
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-03-09 15:45:22 +0100

    DONT MERGE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.javiersc.kotlin.stdlib.notContain
+import org.sonarqube.gradle.SonarExtension
+
 plugins {
     alias(libs.plugins.javiersc.hubdle)
 }
@@ -16,5 +19,17 @@ hubdle {
             site()
         }
         nexus()
+    }
+}
+
+project.allprojects {
+    afterEvaluate {
+        extensions.findByType<SonarExtension>()?.apply {
+            properties {
+                val kotlinFiles = rootDir.walkTopDown().filter { it.extension == "kt" && it.name.notContain("DelegatedProperties") }.map { it.path }.toList()
+                property("sonar.sources", kotlinFiles)
+                property("sonar.tests", kotlinFiles)
+            }
+        }
     }
 }
